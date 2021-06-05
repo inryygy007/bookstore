8867abf91210b4036890eb41892c83730218e738
tangyi2019 <inryygy100@163.com>
2021-06-05 12:01:11 +0800

    初始工程

--- src/main.js
+++ src/main.js
@@ -11,19 +11,10 @@ import 'mint-ui/lib/style.css'
 // import 'element-ui/lib/theme-chalk/index.css'
 import './font/fonts.js'
 import './font/fonts.css'
-// mui
-import './components/lib/mui/css/mui.css'
-import './components/lib/mui/js/mui.min.js'
-import './components/lib/mui/fonts/mui.ttf'
-// 全局组件
-import fenlei from './components/sub/Fenlei.vue'
-import meiwen from './components/sub/Meiwen.vue'
 // axios
 import axios from 'axios'
 axios.defaults.headers.post['Content-type'] = 'application/json'
 Vue.prototype.$axios = axios
-Vue.component('fen-lei', fenlei)
-Vue.component('mei-wen', meiwen)
 // Vue.use(ElementUI)
 Vue.use(Mint)
 Vue.config.productionTip = false
