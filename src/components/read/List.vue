d66c1ea5fad331bbcc46693331ea650eb675b443
tangyi2019 <inryygy100@163.com>
2021-06-06 11:40:59 +0800

    完成加载更多

--- src/components/read/List.vue
+++ src/components/read/List.vue
@@ -1,15 +1,53 @@
 <template>
   <div>
-    List
+    <!-- <h1>{{booklistid}}</h1> -->
+    <lmeiwen :booklist="booklist"></lmeiwen>
   </div>
 </template>
-
 <script>
-  export default {
-    
+import { mapState } from "vuex";
+import { BOOK_PAGE } from "../../store/mutationsType.js";
+import { bookList, bookrecommend } from "../../api/index.js";
+import lmeiwen from "../sub/Lmeiwen/index.vue";
+export default {
+  data() {
+    return {
+      page: 1,
+      id: this.$route.params.id,
+      booklistid: [],
+      booklist: []
+    };
+  },
+  computed: {
+    ...mapState(["header"])
+  },
+  components: {
+    lmeiwen
+  },
+  created() {
+    this.getlist(this.id);
+  },
+  methods: {
+    getlist(id) {
+      if (this.header.headtype === BOOK_PAGE) {
+        bookrecommend(id).then(res => {
+          if (res.status === 200) {
+            this.booklist = res.data.books;
+          }
+        });
+      } else {
+        bookList(id, this.page).then(res => {
+          if (res.data.data.length === 0) {
+            return [];
+          }
+          if (res.status === 200) {
+            res.data.data.forEach(item => {
+              this.booklist.push(item.book);
+            });
+          }
+        });
+      }
+    }
   }
+};
 </script>
-
-<style lang="scss" scoped>
-
-</style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,16 +1,8 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
+import state from './state'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-
-  },
-  mutations: {
-
-  },
-  actions: {
-
-  }
+  state,
 })
